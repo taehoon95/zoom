261be1e22eefb9218fe8f5aaa83cea8211f94c05
taehoon95 <leeth950304@gmail.com>
2021-09-08 14:28:43 +0900

    input안의 메세지를받아서 백엔드로 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const messageList = document.querySelector("ul");  
+const messageForm = document.querySelector("form");
+
 //app.js socket은 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 
@@ -13,6 +16,12 @@ socket.addEventListener("close",() => {
     console.log("Disconnected to Server ❌")
 })
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 1000);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value); // 백엔드에 메세지 전송했고 다음으로 (-> 백엔드에서 메세지 받음 -> 같은 메세지를 다시 전송하고 싶음)
+    input.value = "";
+}
+
+messageForm.addEventListener("submit",handleSubmit)
