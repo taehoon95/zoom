eea84bbdac024b772555422c64c669097be903c3
taehoon95 <leeth950304@gmail.com>
2021-09-08 14:27:19 +0900

    서버에서 연결된 브라우저들로 메시지 보내기

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 //pug 페이지 랜더링
@@ -26,8 +27,12 @@ const wss = new WebSocket.Server({ server }); //내 wss서버에 access
 //백엔드에서 이벤트 socket이 frontend와 실시간으로 소통할 수 있다.
 //server.js의 socket은 연결된 브라우저
 
-//backend와 연결된 각 브라우저에 대해 작동
+// fake database : 누군가 서버에 연결하면, 그 connection을 여기에 넣는다.
+const sockets = [];
+
+//backend와 연결된 각 브라우저에 대해 작동 (연결이 되면 알려주는 부분) , 연결될때 마다 작동(다른 브라우저에서) 
 wss.on("connection", (socket) => {
+    sockets.push(socket); // 연결 될때 마다 sockets 배열에 저장 -> 이렇게 하면 받은 메세지를 다른 모든 socket에 전달 가능
     console.log("Connected to Browser ✅");
 
     //socket에서 event를 listen
@@ -36,11 +41,10 @@ wss.on("connection", (socket) => {
 
     //message 보내기
     socket.on("message", (message) => {
-        console.log(message.toString("utf-8"));
+        //socket.send(message.toString()); // message.toString() 안해주면 /blob값으로 들어간다.
+        sockets.forEach((aSocket) => aSocket.send(message.toString())); // 연결된 브라우저로 모두 보내기 가능
     });
 
-    //연결 되는 순간 socket으로 hello~ 라는 메세지를 보냄
-    socket.send("hello!!!"); 
 });
 
 server.listen(3000, handleListen);
