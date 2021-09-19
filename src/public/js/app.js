e6cf0bb89600ff68ecf9150e172d2daa2cabc790
taehoon95 <leeth950304@gmail.com>
2021-09-19 12:06:22 +0900

    chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,48 +1,92 @@
-const messageList = document.querySelector("ul");  
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
+const socket = io(); // io는 자동적으로 back-end socket.iio와 연결해주는 function 
+                     // 전에는 websocket이 있었는데 지금은 그냥 socketIO 의 socket
+                     // SocketIO는 이미 room 기능이 있음
 
-//app.js socket은 서버로의 연결
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-function makeMessage(type, payload) {
-    const msg = {
-                    type: type,
-                    payload: payload
-                };
-    return JSON.stringify(msg);
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
 }
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-});
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+}
 
-socket.addEventListener("message", (message) => {
-    // 메세지를 받으면 li 태그 생성 -> 생성된 li 태그 ul에 넣는다.
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-}) 
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input"); //querySelector 항상 첫번째것만 가져옴
+    socket.emit("nickname", input.value);
+}
 
-socket.addEventListener("close",() => {
-    console.log("Disconnected to Server ❌")
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
+}
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    // 1. 특정한 event를 emit 
+    // 2. 전송할때 아무거나 전송 가능 (object 포함)
+    // emit 과 on은 같은 이름(같은 String) "enter_room"
+    // 첫번째 argument(여기서는 JSON object로 보냄) : {payload : input.value}
+    // 두번째 argument : function
+    socket.emit("enter_room", { payload:input.value },showRoom); // object를 string으로 변환 시킬 필요 없이 사용가능
+    roomName = input.value;
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+// 방안의 사람수 작성 : welcome과 byed에 반복되는 문장 있어서 만듬 
+function textCount(newCount){
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+}
+
+socket.on("welcome", (user ,newCount) => {
+    textCount(newCount)
+    addMessage(`${user} arrived!`); 
 })
 
-// 메세지 전송용
-function handleSubmit(event){
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value)); // 백엔드에 메세지 전송했고 다음으로 (-> 백엔드에서 메세지 받음 -> 같은 메세지를 다시 전송하고 싶음)
-    input.value = "";
-}
+socket.on("bye", (left ,newCount) => {
+    textCount(newCount)
+    addMessage(`${left} left ㅠㅠ`); 
+})
 
-// 닉네임용
-function handleNickSubmit (event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname",input.value));
-    input.value = "";
-}
+socket.on("new_message", addMessage);
+// (msg) => addMessage(msg) 랑 addMessage 똑같이 작동
 
-messageForm.addEventListener("submit",handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if(rooms.length === 0){
+        return
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;  
+        roomList.append(li)
+    });
+});
